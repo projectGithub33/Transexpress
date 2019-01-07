59bda80cc2b777dfb326036768bd51e73cf64b6a
projectGithub33 <45248655+projectGithub33@users.noreply.github.com>
2019-01-07 02:04:27 +0000

    Create DbStrings.java

---
+++ app/src/main/java/com/example/pc/transexpress/DbStrings.java
@@ -0,0 +1,6 @@
+public class DbStrings {
+    static final String DATABASE_URL="localhost:3306";
+    static final String DATABASE_NAME="mhs_example";
+    static final String USERNAME="root";
+    static final String PASSWORD ="12345678";
+}
