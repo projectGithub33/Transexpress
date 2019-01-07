5b570e41d8acc9c0e9d31ec2064119426cd42d28
projectGithub33 <45248655+projectGithub33@users.noreply.github.com>
2019-01-07 02:08:54 +0000

    Create InfoActivity.java

---
+++ app/src/main/java/com/example/pc/transexpress/InfoActivity.java
@@ -0,0 +1,28 @@
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.widget.TextView;
+
+public class InfoActivity extends AppCompatActivity {
+    TextView t1;
+    TextView t2;
+    TextView t3;
+@Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_info);
+        Bundle extras = getIntent().getExtras();
+
+         t1=(TextView)findViewById(R.id.textView4) ;
+         t2=(TextView)findViewById(R.id.textView5) ;
+         t3=(TextView)findViewById(R.id.textView6) ;
+
+
+        String nom = new String(extras.getString("title"));
+        String solde = new String(extras.getString("solde"));
+        String date = new String(extras.getString("date"));
+
+        t1.setText(nom);
+        t2.setText(solde);
+        t3.setText(date);
+    }
+}
